fbcf3e52be4556ce5184fa2db17809e8e7ad6f57
MuradEminov <murademinov88@gmail.com>
2022-09-09 13:12:24 +0400

    Routing for components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Home from '../src/routes/home/home.component';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation Bar</h1>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop page</h1>;
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route path='home' element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
