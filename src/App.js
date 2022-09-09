348921aefc93d4290b53f76eb48008fe397f4cd4
MuradEminov <murademinov88@gmail.com>
2022-09-09 14:32:59 +0400

    Routes to the respective components set in the parent component. Navigation component with Links done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import Home from '../src/routes/home/home.component';
-import { Routes, Route, Outlet } from 'react-router-dom';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the Navigation Bar</h1>
-        <Outlet />
-      </div>
-    </div>
-  );
-};
+import { Routes, Route } from 'react-router-dom';
+import Navigation from '../src/routes/navigation/navigation.component';
 
 const Shop = () => {
-  return (
-    <div>
-      <h1>I am the shop page</h1>;
-    </div>
-  );
+  return <h1>I am the shop page</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route path='home' element={<Home />} />
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
