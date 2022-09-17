2a631cab4d7012ea9e4cd7bf2a1c0e4b3cf3e0d2
MuradEminov <murademinov88@gmail.com>
2022-09-17 21:11:56 +0400

    Styling of authentication-container div to hold SignIn and SignUpForm components has been done

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,15 +1,9 @@
 // import { useEffect } from 'react';
 // import { getRedirectResult } from 'firebase/auth';
 
-import {
-  auth,
-  signInWithGooglePopup,
-  signInWithGoogleRedirect,
-  createUserDocumentFromAuth,
-} from '../../utils/firebase/firebase.utils';
-
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import './authentication.styles.scss';
 
 const Authentication = () => {
   // useEffect(() => {
@@ -22,14 +16,8 @@ const Authentication = () => {
   //   })();
   // }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup(); // destructed the "response" object
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       {/* <button onClick={signInWithGoogleRedirect}>
         Sign in with Google Redirect
