9bd7cd1324e97f3f91c895e3b2e9079c80bb8ad2
Murad Eminov <murad@murademinov.com>
2022-10-12 13:22:54 +0400

    ProductCard component has been created. Styling for the ProductCard component has been done. ProductCard has been imported into Shopcomponent to render individual products from the shop-data.json

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,14 +4,16 @@ import Button from '../button/button.component';
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
 
-  <div className='product-card-container'>
-    <img src={imageUrl} alt={`${name}`} />
-    <div className='footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
-      <Button type='inverted'>Add to card</Button>
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+        <Button type='inverted'>Add to card</Button>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default ProductCard;
