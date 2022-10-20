a306c162ef34678063540e6e1ae892a3f919947b
Murad Eminov <murad@murademinov.com>
2022-10-20 17:42:05 +0400

    Cart-dropdown component has been created and imported into the Navigation component

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,11 @@
+import './cart-dropdown.styles.scss';
+import Button from '../button/button.component';
+
+const CartDropdown = () => {
+  <div className='cart-dropdown'>
+    <div className='cart-items'></div>
+    <Button>GO TO CHECKOUT</Button>
+  </div>;
+};
+
+export default CartDropdown;
