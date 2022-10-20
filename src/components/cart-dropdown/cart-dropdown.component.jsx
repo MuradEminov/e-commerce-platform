448ae2cad645fa6baf61fa15c054b2886de8dce2
Murad Eminov <murad@murademinov.com>
2022-10-20 19:14:09 +0400

    CartDropdown component has been imported into Navigation component and renders in it

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,10 +2,12 @@ import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 
 const CartDropdown = () => {
-  <div className='cart-dropdown'>
-    <div className='cart-items'></div>
-    <Button>GO TO CHECKOUT</Button>
-  </div>;
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'></div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
 };
 
 export default CartDropdown;
